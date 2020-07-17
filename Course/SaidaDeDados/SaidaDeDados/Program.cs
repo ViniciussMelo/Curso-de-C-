d06e165f7cdcb438402f24878b84fbd46f61fa41
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2020-07-17 13:27:21 -0300

    Initial module

---
+++ Course/SaidaDeDados/SaidaDeDados/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace SaidaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char genero = 'F';
+            int idade = 21;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            //Placeholder
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
+
+            //Interpolação versão C# >= 6
+            //Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+
+            //Concatenação
+            Console.WriteLine(nome + " tem " + idade + " anos" + " e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais");
+
+            /*Console.Write("Olá");
+            Console.WriteLine(", antes sem quebra de linha!");
+            Console.WriteLine("Agora tem");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(nome);
+            Console.WriteLine(saldo.ToString("F2"));//Numero e casas
+            Console.WriteLine(saldo.ToString("F4"));
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));//imprimir com ponto ao invés da vírgula*/
+        }
+    }
+}
---
+++ Course/TiposDeDados/TiposDeDados/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool completo = false;
+            char genero = 'F';//Aspas simples char
+            char letra = '\u0041';///unicode
+            byte n1 = 255;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483647L;
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            string nome = "Vinicius Melo";//string aspas duplas
+            object obj1 = "Vinicius";
+            object obj2 = 4.5f;
+
+            int minValue = int.MinValue;
+            int maxValue = int.MaxValue;
+            sbyte sb = sbyte.MinValue;
+            decimal dec = decimal.MaxValue;
+
+            //Overflow
+            n1++;
+
+            /*Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);*/
+
+            Console.WriteLine(minValue);
+            Console.WriteLine(maxValue);
+            Console.WriteLine(sb);
+            Console.WriteLine(dec);
+        }
+    }
+}
