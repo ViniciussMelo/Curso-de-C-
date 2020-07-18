cb2a3cbfcec9ff02139e043c9c06b51ad32854fe
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2020-07-18 12:45:35 -0300

    Initial module - 2

---
+++ Course/ConversaoImplicitaECasting/ConversaoImplicitaECasting/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConversaoImplicitaECasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conversão implicita
+            float x = 4.5f;
+
+            double y = x;
+
+            Console.WriteLine(y);
+            // -- \\
+
+            //Casting
+            double a;
+            float b;
+
+            a = 5.1;
+            b = (float)a;
+
+            Console.WriteLine(b);
+            // -- \\
+
+
+            double c;
+            int d;
+
+            a = 5.1;
+            d = (int)a;//Perdeu a casa decimal
+
+            Console.WriteLine(d);
+
+            int e = 5;
+            int f = 2;
+
+            //double resultado = e / f;//Compilador entende que a divisão resulta em um inteiro
+            double resultado = (double) e / f;
+
+            Console.WriteLine(resultado);
+
+        }
+    }
+}
---
+++ Course/OperadoresDeAtribuicao/OperadoresDeAtribuicao/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OperadoresDeAtribuicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = a++;//Atribui primeiro e depois incrementa
+            Console.WriteLine("a: {0}", a);
+            Console.WriteLine("b: {0}", b);
+
+            a--;
+            Console.WriteLine(a);
+
+            /*int c = 10;
+
+            Console.WriteLine(c);
+
+            c += 2;
+            Console.WriteLine(c);
+
+            c *= 3;
+            Console.WriteLine(c);
+
+            string s = "ABC";
+            Console.WriteLine(s);
+
+            s += "DEF";
+            Console.WriteLine(s);*/
+
+        }
+    }
+}
