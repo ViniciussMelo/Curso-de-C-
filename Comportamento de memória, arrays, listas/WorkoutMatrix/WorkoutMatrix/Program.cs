ea12291951f8e8813e182648ea3d6e3079ba3cdb
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2020-07-21 20:30:30 -0300

    Add: finished module 'memory, array and list'

--- Comportamento de memória, arrays, listas/WorkoutMatrix/WorkoutMatrix/Program.cs
+++ Comportamento de memória, arrays, listas/WorkoutMatrix/WorkoutMatrix/Program.cs
@@ -10,6 +10,53 @@ namespace WorkoutMatrix
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the number of order: ");
+            int order = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            //lines, columns
+            int[,] matrix = new int[order, order];
+
+            for (int i = 0; i < order; i++)
+            {
+                for (int a = 0; a < order; a++)
+                {
+                    Console.Write("Enter the number of line " + (a + 1) + " and column " + (i + 1) + ": ");
+                    matrix[i, a] = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("Main diagonal: ");
+            for (int i = 0; i < order; i++)
+            {
+                Console.Write(matrix[i, i] + " ");
+            }
+
+            for (int i = 0; i < order; i++)
+            {
+                for (int a = 0; a < order; a++)
+                {
+                    if (matrix[i, a] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nNegative numbers: " + count);
+
+            Console.WriteLine("\n\nMatriz: ");
+
+            for (int i = 0; i < order; i++)
+            {
+                for (int a = 0; a < order; a++)
+                {
+                    Console.Write(matrix[i, a] + " ");
+                }
+                Console.WriteLine("");
+            }
+
         }
     }
 }
