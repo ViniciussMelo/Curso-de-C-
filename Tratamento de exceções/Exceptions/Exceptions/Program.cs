ef8973ea72270d701dd4d5ce74e9355f304bfa36
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2020-07-25 11:03:09 -0300

    Add: module 'Exceptions' and gitignore

---
+++ Tratamento de exceções/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Primeiro número: ");
+                int n1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Segundo número: ");
+                int n2 = int.Parse(Console.ReadLine());
+                
+                int divide = n1 / n2;
+                Console.WriteLine("result" + divide);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division by zero is not allowed");
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Format error! " + e.Message);
+                Console.WriteLine("Format error! " + e.Message);
+            }
+        }
+    }
+}
