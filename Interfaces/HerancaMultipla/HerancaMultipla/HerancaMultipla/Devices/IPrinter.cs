36e4c9e0143db7b08a7931b93658124972cf3674
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2020-08-16 11:18:17 -0300

    Add: finished module 'interfaces'

---
+++ Interfaces/HerancaMultipla/HerancaMultipla/HerancaMultipla/Devices/IPrinter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HerancaMultipla.Devices
+{
+    interface IPrinter
+    {
+        void Print(string document);
+    }
+}
