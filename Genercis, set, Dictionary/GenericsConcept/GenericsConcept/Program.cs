e0eb600ca53e3c581f6727cd88b4fd4a35994f52
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2020-08-18 13:24:46 -0300

    Add: Started module 'Generics, Set, Dictionary'

---
+++ Genercis, set, Dictionary/GenericsConcept/GenericsConcept/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsConcept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<int> printService = new PrintService<int>();
+            //PrintService<string> printService = new PrintService<string>();
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter the " + (i + 1) + "º value:");
+                int x = int.Parse(Console.ReadLine());
+
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
