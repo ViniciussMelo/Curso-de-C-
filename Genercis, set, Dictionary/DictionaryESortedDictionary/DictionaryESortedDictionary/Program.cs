c64c83f34330717f90cb26fee3032db9cd33dd46
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2020-09-05 16:32:33 -0300

    Add: finished modules 'Generics, set, dictionary' and 'Topicos especiais C# II'

---
+++ Genercis, set, Dictionary/DictionaryESortedDictionary/DictionaryESortedDictionary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryESortedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@email.com";
+            cookies["phone"] = "999656587";
+            cookies["phone"] = "132456789";
+
+            Console.WriteLine(cookies["email"]);
+            Console.WriteLine(cookies["phone"]);
+
+            cookies.Remove("email");
+
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            } else
+            {
+                Console.WriteLine("There is no 'email' key");
+            }
+
+            Console.WriteLine("Size: " + cookies.Count);
+
+            Console.WriteLine("ALL COOKIES:");
+            foreach (KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
---
+++ Genercis, set, Dictionary/Workout03/Workout03/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Workout03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            Console.Write("Enter the full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while(!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(",");
+                        string name = line[0].ToUpper();
+                        int quantity = int.Parse(line[1]);
+
+                        if (dictionary.ContainsKey(name))
+                        {
+                            dictionary[name] += quantity;
+                        } else
+                        {
+                            dictionary.Add(name, quantity);
+                        }
+                    }
+                }
+
+                foreach (var item in dictionary)
+                {
+                    Console.WriteLine(item.Key + ": " + item.Value);
+                }
+            }
+            catch (IOException exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+        }
+    }
+}
